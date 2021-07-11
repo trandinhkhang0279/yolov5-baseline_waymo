0b86debc84d8d62e7982ea173ebb3e4ec70ca3e6
Trần Đình Khang <trandinhkhang0279@gmail.com>
2021-07-11 12:28:30 +0700

    Update detect_waymo2021.py

--- detect_waymo2021.py
+++ detect_waymo2021.py
@@ -125,8 +125,8 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
                 p, s, im0, frame = path, '', im0s.copy(), getattr(dataset, 'frame', 0)
 
             p = Path(p)  # to Path
-            save_path = str(save_dir / weights.split(".")[0] / p.name)  # img.jpg
-            txt_path = str(save_dir / weights.split(".")[0] / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
+            save_path = str(save_dir / p.name)  # img.jpg
+            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
             s += '%gx%g ' % img.shape[2:]  # print string
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             imc = im0.copy() if save_crop else im0  # for save_crop
