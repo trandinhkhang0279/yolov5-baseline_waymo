6b34dd064831ddf6311bed0a24c3b7332566d400
Trần Đình Khang <trandinhkhang0279@gmail.com>
2021-07-13 17:41:04 +0700

    Update detect_all_models.sh

--- detect_all_models.sh
+++ detect_all_models.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-python detect_waymo2021.py --weights yolov5s.pt --img 640 --conf 0.25 --source data/content/validation_yolo_format --project pretrained/yolov5s --hide-labels True &
-python detect_waymo2021.py --weights yolov5m.pt --img 640 --conf 0.25 --source data/content/validation_yolo_format --project pretrained/yolov5m --hide-labels True &
-python detect_waymo2021.py --weights yolov5l.pt --img 640 --conf 0.25 --source data/content/validation_yolo_format --project pretrained/yolov5l --hide-labels True &
-python detect_waymo2021.py --weights yolov5x.pt --img 640 --conf 0.25 --source data/content/validation_yolo_format --project pretrained/yolov5x --hide-labels True &
+python detect_waymo2021.py --weights yolov5s.pt --img 640 --conf 0.25 --source data/content/validation_yolo_format --project pretrained/yolov5s --hide-labels --class 0 1 2 7 &
+python detect_waymo2021.py --weights yolov5m.pt --img 640 --conf 0.25 --source data/content/validation_yolo_format --project pretrained/yolov5m --hide-labels --class 0 1 2 7 &
+python detect_waymo2021.py --weights yolov5l.pt --img 640 --conf 0.25 --source data/content/validation_yolo_format --project pretrained/yolov5l --hide-labels --class 0 1 2 7 &
+python detect_waymo2021.py --weights yolov5x.pt --img 640 --conf 0.25 --source data/content/validation_yolo_format --project pretrained/yolov5x --hide-labels --class 0 1 2 7 &
 
-python detect_waymo2021.py --weights yolov5s6.pt --img 1280 --conf 0.25 --source data/content/validation_yolo_format --project pretrained/yolov5s6 --hide-labels True &
-python detect_waymo2021.py --weights yolov5m6.pt --img 1280 --conf 0.25 --source data/content/validation_yolo_format --project pretrained/yolov5m6 --hide-labels True &
-python detect_waymo2021.py --weights yolov5l6.pt --img 1280 --conf 0.25 --source data/content/validation_yolo_format --project pretrained/yolov5l6 --hide-labels True &
-python detect_waymo2021.py --weights yolov5x6.pt --img 1280 --conf 0.25 --source data/content/validation_yolo_format --project pretrained/yolov5x6 --hide-labels True
+python detect_waymo2021.py --weights yolov5s6.pt --img 1280 --conf 0.25 --source data/content/validation_yolo_format --project pretrained/yolov5s6 --hide-labels --class 0 1 2 7 &
+python detect_waymo2021.py --weights yolov5m6.pt --img 1280 --conf 0.25 --source data/content/validation_yolo_format --project pretrained/yolov5m6 --hide-labels --class 0 1 2 7 &
+python detect_waymo2021.py --weights yolov5l6.pt --img 1280 --conf 0.25 --source data/content/validation_yolo_format --project pretrained/yolov5l6 --hide-labels --class 0 1 2 7 &
+python detect_waymo2021.py --weights yolov5x6.pt --img 1280 --conf 0.25 --source data/content/validation_yolo_format --project pretrained/yolov5x6 --hide-labels --class 0 1 2 7 
